909004317d50ec29f20d8a30809d35e2f9e6a3dd
carlos-perez4 <careduar815@gmail.com>
2023-06-23 08:28:46 -0600

    Version1
    esta primer subida de archivos.

---
+++ datos.php
@@ -0,0 +1,8 @@
+<?php
+//$nombre = $_GET ['txtNombre'];
+$nombre = $_POST ['txtNombre'];
+//$apellidos = $_GET ['txtApellidos'];
+$apellidos = $_POST ['txtApellidos'];
+
+echo $nombre . "/" . $apellidos;
+?>
